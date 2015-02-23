a3be7a5e7c2c8125f733525c0cd1758948e31bf8
Victor Pereira <vpereira@suse.de>
2015-02-23 11:14:06 +0100

    added some hint how to run it

--- plugins/file_system_check.rb
+++ plugins/file_system_check.rb
@@ -8,7 +8,7 @@ module SecCheck
       # TODO
       # implement the logic
       def check_var_spool_mail
-        @db.select { |k,v| k.match /^\/var\/spool\/mail/ }.each do |k,v|
+        @db.select { |k,v| k.match /\/var\/spool\/mail/ }.each do |k,v|
           if v[:owner] != File.basename(k)
             puts "WARNING: #{k} owned by #{v[:owner]}"
           end
--- seccheck.rb
+++ seccheck.rb
@@ -31,15 +31,20 @@ require_relative 'plugins/file_system_check'
 require_relative 'plugins/rpm_md5_check'
 
 if __FILE__ == $0
+
+   if ARGV[0].nil?
+     puts "#{$0} <base-dir>"
+     exit 1
+   end
    # retrieving:
     #old_hash_fs = File.open("test.marshal", "r"){|from_file| Marshal.load(from_file)}
-    old_hash_fs = JSON.load(File.read("test.json")) # we could just suck it up, but pass it through load,
-    #works as sanity check
+    old_hash_fs = JSON.load(File.read("test.json")) # we could just suck it up,
+    #but pass it through load,works as sanity check
     fs_scanner = SecCheck::FSScanner.new ARGV[0]
     hash_fs = fs_scanner.search
     JsonCompare.get_diff(old_hash_fs.to_json,hash_fs.to_json)
     SecCheck::FileSystemCheck.new(hash_fs[:files]).check_var_spool_mail
     #puts SecCheck::ConfigDiff.new(old_hash_fs,hash_fs).run
     #File.open("test.marshal", "w"){|to_file| Marshal.dump(hash_fs, to_file)}
-    #File.open("test.json", "w"){ |to_file| to_file.write(hash_fs.to_json)}
+    File.open("test.json", "w"){ |to_file| to_file.write(hash_fs.to_json)}
  end
